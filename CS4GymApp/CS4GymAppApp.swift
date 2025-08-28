6ea58a642f384cefb9cf2eb4a29dd3ac2a99f39e
munhty <mhtran483@gmail.com>
2025-08-28 12:04:26 -0500

    Initial Commit

---
+++ CS4GymApp/CS4GymAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CS4GymAppApp.swift
+//  CS4GymApp
+//
+//  Created by Minh Tran  on 8/28/25.
+//
+
+import SwiftUI
+
+@main
+struct CS4GymAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CS4GymAppTests/CS4GymAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  CS4GymAppTests.swift
+//  CS4GymAppTests
+//
+//  Created by Minh Tran  on 8/28/25.
+//
+
+import Testing
+@testable import CS4GymApp
+
+struct CS4GymAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
